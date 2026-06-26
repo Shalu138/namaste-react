5fdd9974833123c8fe27810fad0200ede06a5d99
Shalu138 <shalushah138@gmail.com>
2026-06-26 16:01:53 +0530

    props and destructring - ep4

--- App.js
+++ App.js
@@ -91,5 +91,89 @@ const HeadingComponent2 = () => (
   </div>
 );
 
+// *************** FOOD DELIVERY APP *****************
+const Header = () => {
+  // inline styling - just like creating an object
+  const headerStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  };
+  return (
+    <div className="headerMain">
+      <div className="container" style={headerStyle}>
+        <a href="/" className="logo">
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/005/513/590/small/catering-quality-food-design-premium-logo-vector.jpg"
+            alt="Logo"
+          />
+        </a>
+        <div className="nav-right">
+          <a href="javascript:void(0)" className="nav-item">
+            Home
+          </a>
+          <a href="javascript:void(0)" className="nav-item">
+            About
+          </a>
+          <a href="javascript:void(0)" className="nav-item">
+            Restaurants
+          </a>
+          <a href="javascript:void(0)" className="nav-item">
+            Cart
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  console.log(props.resName);
+  return (
+    <div className="res-card">
+      <img
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/3/26/a260a21f-1b2c-4690-998a-695887bbbd1f_289294.jpg"
+        alt="restaurant-image"
+        className="res-img"
+      />
+      <h4 className="res-name">{props.resName}</h4>
+      <div>{props.cusine}</div>
+      <div>4 Star</div>
+      <div>25-30 min</div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="main-wrapper">
+      <div className="container">
+        <div className="search-bar">
+          <form>
+            <input type="text" />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+
+        <div className="restaurant-wrapper">
+          <div className="restaurant-row">
+            <RestaurantCard resName="Box 8" cusine="North India" />
+            <RestaurantCard resName="Specia Bistro" cusine="Italian" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  ); // return is important. But why?
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent2 />);
+root.render(<AppLayout />);
