5e2d59781c6ffeaf0daf84ef83ce113cbb702631
Shalu138 <shalushah138@gmail.com>
2026-07-07 12:51:10 +0530

    list of restaurant (array)

--- App.js
+++ App.js
@@ -128,31 +128,108 @@ const Header = () => {
 };
 
 // Object created for Restaurant
-const resObj = {
-  type: "restaurant",
-  data: {
-    type: "V",
-    id: "1234",
-    name: "BOX 8",
-    costForTwo: "400",
-    avgRating: "3.6",
-    cusine: ["North Indian", "Indian"], //array
+const resList = [
+  {
+    type: "restaurant",
+    data: {
+      id: "1001",
+      name: "BOX8 - Desi Meals",
+      cuisine: "North Indian, Biryani",
+      typeOfRes: "Cloud Kitchen",
+      costForTwo: "400",
+      avgRating: "4.3",
+      deliveryTime: "25-30 min",
+      image:
+        "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/3/26/a260a21f-1b2c-4690-998a-695887bbbd1f_289294.jpg",
+    },
   },
-};
+  {
+    type: "restaurant",
+    data: {
+      id: "1002",
+      name: "La Pino'z Pizza",
+      cuisine: "Italian, Pizzas, Fast Food",
+      typeOfRes: "Casual Dining",
+      costForTwo: "500",
+      avgRating: "4.1",
+      deliveryTime: "20-25 min",
+      image:
+        "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/f8334117211ec4e74406caebec280995",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      id: "1003",
+      name: "Barbeque Nation",
+      cuisine: "North Indian, Barbeque, Kebabs",
+      typeOfRes: "Buffet / Fine Dining",
+      costForTwo: "1500",
+      avgRating: "4.5",
+      deliveryTime: "40-45 min",
+      image:
+        "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/th67be72ccbc62a875a6cebec280995",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      id: "1004",
+      name: "Chai Point",
+      cuisine: "Bakery, Beverages, Cafe",
+      typeOfRes: "Cafe / Quick Service",
+      costForTwo: "250",
+      avgRating: "3.9",
+      deliveryTime: "15-20 min",
+      image:
+        "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/bpx34117211ec4e74406caebec280995",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      id: "1005",
+      name: "Wow! Momo",
+      cuisine: "Tibetan, Chinese, Fast Food",
+      typeOfRes: "Quick Service Restaurant",
+      costForTwo: "350",
+      avgRating: "4.2",
+      deliveryTime: "20-30 min",
+      image:
+        "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/9924a4117211ec4e74406caebec280995",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      id: "1006",
+      name: "Sagar Ratna",
+      cuisine: "South Indian, Pure Veg",
+      typeOfRes: "Family Restaurant",
+      costForTwo: "600",
+      avgRating: "4.4",
+      deliveryTime: "30-35 min",
+      image:
+        "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/sr834117211ec4e74406caebec280995",
+    },
+  },
+];
+
+console.log(resList);
 
 const RestaurantCard = (props) => {
   const { resData } = props;
   return (
     <div className="res-card">
       <img
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/3/26/a260a21f-1b2c-4690-998a-695887bbbd1f_289294.jpg"
+        src={resData.data.image}
         alt="restaurant-image"
         className="res-img"
       />
       <h4 className="res-name">{resData.data.name}</h4>
       <div>Rs. {resData.data.costForTwo}</div>
       <div>{resData.data.avgRating} Star</div>
-      <div>{resData.data.cusine.join(", ")}</div>
+      <div>{resData.data.cuisine}</div>
     </div>
   );
 };
@@ -170,7 +247,8 @@ const Body = () => {
 
         <div className="restaurant-wrapper">
           <div className="restaurant-row">
-            <RestaurantCard resData={resObj} />
+            <RestaurantCard resData={resList[0]} />
+            <RestaurantCard resData={resList[1]} />
             {/* <RestaurantCard resName="Specia Bistro" cusine="Italian" /> */}
           </div>
         </div>
